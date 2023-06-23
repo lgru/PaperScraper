67f818edbb25b2f585b5fb684e95a846fefe9878
grundl89 <csm4st3rk1ng>
2023-06-23 18:20:09 +0200

    Adding further dummy data for citation, reference, and recommendation tables.

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,13 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable2').DataTable();
+});
+
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable3').DataTable();
+});
